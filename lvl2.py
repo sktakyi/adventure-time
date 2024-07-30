2b7c4f606a660935962e37f5b407bbe433735514
sktakyi <s.k.takyi5@gmail.com>
2024-07-30 18:07:45 +0000

    Exp.100

---
+++ lvl2.py
@@ -0,0 +1,13 @@
+#Function Simplicity Practice
+def greet() -> None:
+    print('Hello, User!')
+
+def bye() -> None:
+    print('Bye, User')
+
+def main() -> None:
+    greet()
+    bye()
+ 
+if __name__ == 'main':
+    main()
