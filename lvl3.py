db1f8eada2d10c28169e2d8305757d9edf007706
sktakyi <s.k.takyi5@gmail.com>
2024-07-31 04:11:47 +0000

    Exp.68

---
+++ lvl3.py
@@ -0,0 +1,11 @@
+#Looping Safe Practice - do not loop orginal list
+items: list[str] = ['A', 'B', 'C', 'D']
+new_items: list[str] = []
+
+for item in items:
+    if item == 'B':
+        continue
+    else:
+        print(item)
+        new_items.append(item)
+        
