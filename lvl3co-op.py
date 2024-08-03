d4a3df2b7f011c7d453861714a840444cb559067
sktakyi <s.k.takyi5@gmail.com>
2024-08-03 01:16:45 +0000

    Exp.93

--- lvl3co-op.py
+++ lvl3co-op.py
@@ -1,4 +1,4 @@
 #list nuggets
-countries: list[str] = ['USA', 'UK', 'SPAIN', 'GERMANY', 'HOBBY']
+countries: list[str] = ['USA', 'UK', 'SPAIN', 'GERMANY', 'COUNTRY']
 countries[4] = 'CANADA'
 print(countries[0:]) #prints all values in list, id can also be used
